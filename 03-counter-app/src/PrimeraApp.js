ff4a96e12c76d3982f6ca8c0b6dc75062eb69e08
Erick Díaz <erickdp@hotmail.com>
2022-04-16 22:30:30 -0500

    Uso de defaultProps

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 // Funcional Components
 
-const PrimeraApp = ({ saludo = "Hola mundo" }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
     // const saludo = "Hola mundo const";
     const persona = {
         nombre: "Erick",
@@ -12,13 +12,18 @@ const PrimeraApp = ({ saludo = "Hola mundo" }) => {
         <h1>{saludo}</h1>
         {/* permite imprimir un objeto con Json.stringify */}
         <pre>{JSON.stringify(persona, null, 3)}</pre>
-        <p>Mi primera App</p>
+        <p>{subtitulo}</p>
     </>
 }
 
 PrimeraApp.propTypes = {
     // defino que saludo sea de tipo string y que sea requerido
     saludo: PropTypes.string.isRequired
+}
+
+// Para definir valores de los pros por defecto
+PrimeraApp.defaultProps = {
+    subtitulo: "Soy un subtítulo"
 }
 
 export default PrimeraApp;
