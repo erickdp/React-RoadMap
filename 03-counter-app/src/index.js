5b91fb78a344b3e09ae0615232ca3780770f624e
Erick Díaz <erickdp@hotmail.com>
2022-04-16 23:09:31 -0500

    Paso de referencia de método

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDom from "react-dom";
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+// import PrimeraApp from './PrimeraApp';
 
 import "./index.css"
 // const saludo = <h1>Hola mundo</h1>;
 
 const divRoot = document.querySelector("#root");
 
-ReactDom.render(<PrimeraApp saludo='Hola Mundoo' />, divRoot);
+ReactDom.render(<CounterApp value={23} />, divRoot);
 
 
