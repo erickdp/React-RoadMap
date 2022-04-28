e0a54340a3f89a4e7c220c2d06ef0f002f1a298a
Erick Díaz <erickdp@hotmail.com>
2022-04-28 12:22:15 -0500

    manejo de useEffect

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const GifGrid = ({ categoria }) => {
 
     const [count, setCount] = useState(0)
 
+    // use effect evita bucles infinitos de renderizado y solo lo aplica esto solo al cambiar la función contenida 
+    useEffect(() => {
+        getGifs();
+    }, []) // lista vacía significa que solo se renderiza esa función una sola vez
 
     const getGifs = async () => {
         const url = "https://api.giphy.com/v1/gifs/search?q=baki&limit=10&api_key=Z99vhhgJKRwA8cP8syjO5feAu9JNhUEx";
@@ -20,8 +24,6 @@ const GifGrid = ({ categoria }) => {
 
         console.log(gifts);
     }
-
-    getGifs();
 
     return (
         <div>
